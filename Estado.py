0e1ae3097956ca478229dc47e9c77955e0818717
ronyajtun <raab2010173@gmail.com>
2014-09-26 17:05:33 -0600

    Update Estado.py

--- Estado.py
+++ Estado.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 import tweepy
+import RPi.GPIO as GPIO
+
+#Puertos de entrada y salida
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+GPIO.setwarnings(False)
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(27,GPIO.OUT)
 
 #Coloca dentro de las comillas tus claves...
 CONSUMER_KEY = 'S9Tw7CnZCRWtjzPC0ZLzmKwwq'
@@ -14,3 +22,7 @@ auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 #update_status('mensaje' o variable) es para actualizar nuestro estado
 x = tweepy.API(auth)
 x.update_status('Publique mi primer tweet ')
+
+print "Lights on"
+GPIO.output(17,GPIO.HIGH)
+GPIO.output(27,GPIO.HIGH) 
